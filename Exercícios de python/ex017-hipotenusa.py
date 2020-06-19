75648123488804b4f6ff58d113f8e5fa9dae995f
Vinícius Lima <66859628+viniciusflima@users.noreply.github.com>
2020-06-19 19:38:02 -0300

    Update ex017-hipotenusa.py

--- Exercícios de python/ex017-hipotenusa.py
+++ Exercícios de python/ex017-hipotenusa.py
@@ -1,11 +1,12 @@
 # le comprimento dos catetos e calcula a hipotenusa
-#
-# from math import sqrt, pow
-# cat1 = float(input('Insira um dos lados do triângulo: '))
-# cat2 = float(input('Insira o outro lado: '))
-# print(f'A hipotenusa desse triângulo vale {sqrt(pow(cat1, 2)+pow(cat2, 2)):.2f}')
-#
-"""uma outra forma é incluir o método hypot(cat1, cat2)"""
+
+""" from math import sqrt, pow
+cat1 = float(input('Insira um dos lados do triângulo: '))
+cat2 = float(input('Insira o outro lado: '))
+print(f'A hipotenusa desse triângulo vale {sqrt(pow(cat1, 2)+pow(cat2, 2)):.2f}')"""
+
+# uma outra forma é incluir o método hypot(cat1, cat2)
+
 from math import hypot
 co = float(input('Digite o cateto oposto: '))
 ca = float(input('Digite o cateto adjacente: '))
