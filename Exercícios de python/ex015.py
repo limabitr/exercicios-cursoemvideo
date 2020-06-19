cb8f58939c5194c164b23274d8d6d8a43b477db1
Vinícius Lima <66859628+viniciusflima@users.noreply.github.com>
2020-06-18 23:16:03 -0300

    Update and rename ex015.py to ex015-questao.py

--- Exercícios de python/ex015.py
+++
@@ -1,8 +0,0 @@
-# Programa que lê a quantidade de Km percorridos por um carro
-# alugado e a qnt de dias pelos quais foi alugado.
-# Calcule o preço a pagar, sabendo que o carro custa R$60
-# por dia e R$0.15 por Km rodado
-#
-km = float(input('Quantos Km foram percorridos? '))
-d = int(input('Por quantos dias o carro ficou alugado? '))
-print(f'O valor correspondente ao seu aluguel foi de R$ {(d*60+km*0.15):.2f}')
