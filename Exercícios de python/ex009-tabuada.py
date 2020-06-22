aac208927c81e60d6975f73e05589a1ec6bdd0d2
Vinícius Lima <vinicius.limaadcg@hotmail.com>
2020-06-22 18:42:41 -0300

    Update ex009-tabuada.py

--- Exercícios de python/ex009-tabuada.py
+++ Exercícios de python/ex009-tabuada.py
@@ -1,8 +1,8 @@
-# Programa que lê um número inteiro e mostra a sua tabuada
+# Programa que lê um número inteiro e mostra a sua tabuada. A sintaxe \033[;m indica código de cor. Ignore.
 a = str('TABUADA')
-print(f'{a:=^40}')
+print(f'\033[33m {a:=^40}\033[m')
 n = int(input('Digite um número inteiro: '))
-print(f'{n} x {1:2} = {(n*1):2}\n'
+print(f'\033[36m{n} x {1:2} = {(n*1):2}\n'
       f'{n} x {2:2} = {(n*2):2}\n'
       f'{n} x {3:2} = {(n*3):2}\n'
       f'{n} x {4:2} = {(n*4):2}\n'
@@ -11,5 +11,5 @@ print(f'{n} x {1:2} = {(n*1):2}\n'
       f'{n} x {7:2} = {(n*7):2}\n'
       f'{n} x {8:2} = {(n*8):2}\n'
       f'{n} x {9:2} = {(n*9):2}\n'
-      f'{n} x 10 = {(n*10):2}\n'
+      f'{n} x 10 = {(n*10):2} \033[m'
       )
