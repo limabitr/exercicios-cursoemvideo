f5a1a2d6efe41e765c2d684db00437783aa3aa11
Vinícius Lima <vinicius.limaadcg@hotmail.com>
2020-06-22 18:33:57 -0300

    Add colours

--- Exercícios de python/ex007-media.py
+++ Exercícios de python/ex007-media.py
@@ -1,6 +1,6 @@
 # Programa que lê notas de um aluno e mostra sua média
-n = int(input('Digite sua nota do primeiro bimestre: '))
-n2 = int(input('Digite sua nota do segundo bimestre: '))
-n3 = int(input('Digite sua nota do terceiro bimestre: '))
-n4 = int(input('Digite sua nota do quarto bimestre: '))
-print(f'Sua média é {((n+n2+n3+n4)/4):.1f}')
+n = int(input('\033[35mDigite sua nota do primeiro bimestre: \033[m'))
+n2 = int(input('\033[35mDigite sua nota do segundo bimestre: \033[m'))
+n3 = int(input('\033[35mDigite sua nota do terceiro bimestre: \033[m'))
+n4 = int(input('\033[35mDigite sua nota do quarto bimestre: \033[m'))
+print(f'\033[1;36mSua média é {((n+n2+n3+n4)/4):.1f}\033[m')
