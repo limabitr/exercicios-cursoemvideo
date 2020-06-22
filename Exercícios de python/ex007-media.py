730ef33a1a3e18f49965a8a40e0e5ee7dde10477
Vinícius Lima <66859628+viniciusflima@users.noreply.github.com>
2020-06-22 18:44:38 -0300

    Update ex007-media.py

--- Exercícios de python/ex007-media.py
+++ Exercícios de python/ex007-media.py
@@ -1,6 +1,6 @@
-# Programa que lê notas de um aluno e mostra sua média
-n = int(input('\033[35mDigite sua nota do primeiro bimestre: \033[m'))
-n2 = int(input('\033[35mDigite sua nota do segundo bimestre: \033[m'))
-n3 = int(input('\033[35mDigite sua nota do terceiro bimestre: \033[m'))
-n4 = int(input('\033[35mDigite sua nota do quarto bimestre: \033[m'))
-print(f'\033[1;36mSua média é {((n+n2+n3+n4)/4):.1f}\033[m')
+# Programa que lê notas de um aluno e mostra sua média. \033[m é um código para cores.
+n = int(input('\033[35m Digite sua nota do primeiro bimestre: \033[m'))
+n2 = int(input('\033[35m Digite sua nota do segundo bimestre: \033[m'))
+n3 = int(input('\033[35m Digite sua nota do terceiro bimestre: \033[m'))
+n4 = int(input('\033[35m Digite sua nota do quarto bimestre: \033[m'))
+print(f'\033[1;36m Sua média é {((n+n2+n3+n4)/4):.1f} \033[m')
