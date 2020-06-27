5d79fad00ca73cb95e8c00470a953ff9d6c60da2
Vinícius Lima <vinicius.limaadcg@hotmail.com>
2020-06-26 23:44:50 -0300

    Create ex040-media.py

---
+++ Exercícios de python/ex040-media.py
@@ -0,0 +1,10 @@
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1+n2)/2
+
+if m < 5.0:
+    print('\033[1;31mREPROVADO\033[m')
+elif 5.0 <= m < 6.0:
+    print('\033[1;33mRECUPERAÇÃO\033[m')
+elif m >= 7.0:
+    print('\033[1;34mAPROVADO\033[m')
